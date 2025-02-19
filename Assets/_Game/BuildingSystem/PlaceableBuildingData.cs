a03f312b11bd1d5bbc97f33032471d6d129577ce
Kskefeafyonlu <karsiyakaefe@icloud.com>
2025-02-19 21:05:10 +0300

    v0.0.3

---
+++ Assets/_Game/BuildingSystem/PlaceableBuildingData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace _Game.BuildingSystem
+{
+    public class PlaceableBuildingData : ScriptableObject
+    {
+        public string buildingName;
+    
+        public float buildingWidth = 1f;
+        public float buildingHeight = 1f;
+    
+    
+    }
+}
